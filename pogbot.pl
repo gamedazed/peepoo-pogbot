f3ffd06e6bbf0d47e7d0611f9c8fc9239ee364b9
gamedazed <gamedazed@penguinnet.io>
2023-06-14 14:31:29 -0500

    google drive path to include the subfolder in dest

--- pogbot.pl
+++ pogbot.pl
@@ -5,15 +5,15 @@ use warnings;
 
 ###################################################################################################
 #####################################  Written by gamedazed   #####################################
-#                                   ...^~77?JJ?7?YYYYYJYYJJ?~^::..                                    
-#                          .::~7J5PBB###&&&&&&&&&&&&&&&&&&&&&#####BGPY?!^:.                           
-#                  ..^~7YPB##&&######BBBBBBBGBBBBBBBBBBB###BBB####&&&&&&&&#BPY?!^.                    
-#             :~?5B#&&&&&&#####BBBBGGPP5PPPPP5PPPGGGGGGGGGPPGGGGGGGGBBB##&&&&&&&&#BP?~.               
-#         .~YG#&&&&&&##BBGGBBBBBGP55YY5555YYYYYY55PPP555P55PPPPPPPPPPGGGGBBB###&&&&&&&&#GJ~.          
-#      .:7PB#&&####BBBBBBBBBBGY7~^^:^~7JYYYYYY55YY5555555555555JJJY5GBBBBGGGGGBBBBBB##&&&&&#GJ^       
-#    :?PBB###BBBGGGGGGGP5YYJ!.       .~YPPGGGGGGPPGGGGGPP5Y7!^.    ..^?PB#BBBBGGGGGGBBBBGGBB###5!.    
-#  :?GBB###BG5YYJJJJJJJJJJJJJ?7!~~!?YPB#&#################BPJ~..   ....^7Y5PPGGGGGGBBGGP5555GB#&#5^   
-# 5BBBBB#BG5J7~^:^!?Y55PGGGGGGGGGB#&&&#BGPYYJJJJY55PBB###&###BGPYJ??JY5PGGGGPPPPPPPGGGGGP5J?JYPB#&B7  
+#                                   ...^~77?JJ?7?YYYYYJYYJJ?~^::..
+#                          .::~7J5PBB###&&&&&&&&&&&&&&&&&&&&&#####BGPY?!^:.
+#                  ..^~7YPB##&&######BBBBBBBGBBBBBBBBBBB###BBB####&&&&&&&&#BPY?!^.
+#             :~?5B#&&&&&&#####BBBBGGPP5PPPPP5PPPGGGGGGGGGPPGGGGGGGGBBB##&&&&&&&&#BP?~.
+#         .~YG#&&&&&&##BBGGBBBBBGP55YY5555YYYYYY55PPP555P55PPPPPPPPPPGGGGBBB###&&&&&&&&#GJ~.
+#      .:7PB#&&####BBBBBBBBBBGY7~^^:^~7JYYYYYY55YY5555555555555JJJY5GBBBBGGGGGBBBBBB##&&&&&#GJ^
+#    :?PBB###BBBGGGGGGGP5YYJ!.       .~YPPGGGGGGPPGGGGGPP5Y7!^.    ..^?PB#BBBBGGGGGGBBBBGGBB###5!.
+#  :?GBB###BG5YYJJJJJJJJJJJJJ?7!~~!?YPB#&#################BPJ~..   ....^7Y5PPGGGGGGBBGGP5555GB#&#5^
+# 5BBBBB#BG5J7~^:^!?Y55PGGGGGGGGGB#&&&#BGPYYJJJJY55PBB###&###BGPYJ??JY5PGGGGPPPPPPPGGGGGP5J?JYPB#&B7
 # BBGGBBBGPY?~::^?PGBBBB########&&#BPJ~:.        ...:!YPGBB###BBBBBBBBBB####BBBBBBBBBGGP5YJ??JY5PB&#5:
 # BBGGGPGBPJ!^::7PB##########&&&#BGPY^               .!JYPGB#####BBBB################BGPY?~~~!7?YPB#&G
 # #BBG5PBB57~::^JPBB#######&&&&#BGP5Y?^            .:!JY5PGBBB##############&&&&&&&##BG5J!^:^^~!7J5B&#
@@ -22,13 +22,13 @@ use warnings;
 # .7PGBBBPY!^:^!??JJYPGBGG5Y7~:.....^:...           ..:^!!?JYYJ!^:...~?5B###########BBBGGPP5!~!!7?JY5P
 #  :75PPYJ77!!777?JY5PGGGGPPPY?^.   .^:^:....     ...^^~~:!7:.   .:!YPGB######BBBBBBBBBBBGGPJ!!!7?Y5GG
 #   ..:~J5P55YJJJJYY5555PPPPP55Y?~.. ^7~^::::. ...::^!??^:.  .:~JPGB######BBBBBBBGBBBB####BBP!..:~7Y5P
-#     .~YGBBGP55YY55555555555555P57:^7!~^^^^^:.:::^^~7Y7 ..^7YPBBB######BBBBBBBBBBBBB######5!.     .. 
-#      .^?5GBBGGPPPP55555555555YYPP77!~~^^^^^::^^^^~~7J~.~YPBBBB########BBBBBBGGGBBB#####BY^          
-#      .~?PBBBBGGGGPPP55555555YY5PGGJ!~~^^^~^^^^^^~~!7Y55GBBBBB#####BBBBBBGGGGGGGBBB####5^.           
-#      .^JG###BBBBBGGGPPPP555YYYY55GGPJ7!~~~^^^^~~!?YPBBBBBBBGBBBBGGGGGGGGGPPPGGBB#####BY^            
-#    .^JG######BBBGGGGPP55YYYYYY55GBB#BP5YYYYY555PGGGGGGBBBBGGGPPPPPPPGGGGGGGGGBBB###&&&BJ^           
-# ^!JG#&&&#########BBGGPPP5555PPGGGGGPPPPGGBBBBBBBGGGGPPGPPGGGPPPPPGGGBBBBBBBBBB#####&&&&P!.          
-# G#&&&&###BBBBBBBBBBGGGGGGGGPPPP55PPPGGGBBBBGPGGBGGGP5555PGGGGGGGGGGGBBBBB##########&&&&&BY^.        
+#     .~YGBBGP55YY55555555555555P57:^7!~^^^^^:.:::^^~7Y7 ..^7YPBBB######BBBBBBBBBBBBB######5!.     ..
+#      .^?5GBBGGPPPP55555555555YYPP77!~~^^^^^::^^^^~~7J~.~YPBBBB########BBBBBBGGGBBB#####BY^
+#      .~?PBBBBGGGGPPP55555555YY5PGGJ!~~^^^~^^^^^^~~!7Y55GBBBBB#####BBBBBBGGGGGGGBBB####5^.
+#      .^JG###BBBBBGGGPPPP555YYYY55GGPJ7!~~~^^^^~~!?YPBBBBBBBGBBBBGGGGGGGGGPPPGGBB#####BY^
+#    .^JG######BBBGGGGPP55YYYYYY55GBB#BP5YYYYY555PGGGGGGBBBBGGGPPPPPPPGGGGGGGGGBBB###&&&BJ^
+# ^!JG#&&&#########BBGGPPP5555PPGGGGGPPPPGGBBBBBBBGGGGPPGPPGGGPPPPPGGGBBBBBBBBBB#####&&&&P!.
+# G#&&&&###BBBBBBBBBBGGGGGGGGPPPP55PPPGGGBBBBGPGGBGGGP5555PGGGGGGGGGGGBBBBB##########&&&&&BY^.
 ###################################################################################################
 
 use lib q{/home/gamedazed/modules};
@@ -106,7 +106,7 @@ sub poll() {
             my $channel_name = $streams{pid}{$pid}{channel};
             next if $channel_name eq 'Parent';
             &PeePoo::printl(q{info}, qq{$channel_name ($pid)});
-        }        
+        }
         # Print files modified over the past 1800 minutes
         &PeePoo::printxl(q{echo && find /nas/videos/Captures/ -type f -mmin -1800 -exec ls -l {} \;}) if qx{ps aux | grep yt-dlp | grep -v grep | wc -l | tr -d "\n"};
         # Show actively running yt-dlp processes
@@ -175,7 +175,7 @@ sub get_chatdownload_cmd() {
     my $outputDir    = shift;
     my $outputFile   = shift;
     my $vod_id       = shift;
-    # my $cmd = qq{docker run --rm -it }            
+    # my $cmd = qq{docker run --rm -it }
     # . qq{--name "$channel_name-peepootwitchybot" }
     # . qq{-e running_application='twitch-downloader-cli' }
     # . qq{--network 'shitting_and_farting' -v nas_share:/nas/ }
@@ -202,7 +202,7 @@ sub get_chatrender_cmd() {
     # . qq{--network 'shitting_and_farting' -v nas_share:/nas/ }
     # . qq{ bxggs/twitch-downloader-cli:latest };
     my $cmd = q{TwitchDownloaderCLI };
-    
+
     my $trigger_command = qq{chatrender }
     . qq{-i $outputDir/'$chatIn' }
     . qq{--outline --font-size 17 --skip-drive-waiting }
@@ -226,7 +226,7 @@ sub get_vod_cmd() {
     # xychelsea/ffmpeg-nvidia:latest                                 \\
     my $cmd = q{ffmpeg};
     if ($useHW_Encoding){
-        my $trigger_command = 
+        my $trigger_command =
         qq{ -y -vsync 2  -hwaccel cuda -i $outputDir/'$video'        \\
         -i $outputDir/'$chat'                                        \\
         -filter_complex hstack                                       \\
@@ -253,7 +253,7 @@ sub live_trigger() {
     chomp $channel_name;
 
     my $outputDir = $localMntPoint . $localOutPath . $channel_name; # i.e. /nas/videos/Captures/lordaethelstan
-    foreach my $dir ($outputDir, qq{$gcsMountPoint/$channel_name}) {
+    foreach my $dir ($outputDir, qq{$gcsMountPoint/$gcsBucketName}) {
         print qx{mkdir -vp $dir} unless -d $dir;
     }
     my $timestamp = &PeePoo::timestamp(q{yyyy_mm_dd-hh:mm:ss});     # i.e. 2023_05_30-16:22:30
@@ -267,7 +267,7 @@ sub live_trigger() {
         &PeePoo::printl(q{error}, qq{Exiting out early - YT-DLP returned an error\n\--- $liveRecord_returnCode ---\n$liveRecord_executionStatus});
         return qq{Failed with code $liveRecord_returnCode\n}
     }
-    
+
     my $video = &get_fn($outputDir, q{.mp4});
     $video =~ s/_chat\.mp4$/\.mp4/; # if a chat file was the most recently written
     &PeePoo::printl(q{notice}, qq{    * Filename: $video});
@@ -293,11 +293,11 @@ sub live_trigger() {
           &PeePoo::printl(q{critical}, qq{ VOD ID returned $vod_id - I'm going to try to download chat with this but I don't think it looks right\n}) :
           &PeePoo::printl(q{critical}, qq{ VOD ID returned undefined! Cannot download chat!\n});
         # If you don't have a VOD ID, as can happen when vods aren't saved on a channel, just move the vod recording to the cloud storage
-        &PeePoo::printxl(qq{mv -v $video $gcsMountPoint/$channel_name/$video});
+        &PeePoo::printxl(qq{mv -v $video $gcsMountPoint/$gcsBucketName/$channel_name/$video});
         #&post_notification($channel_name, $video);
         return 154;
     }
-    
+
     my $chatDownloadCmd = &get_chatdownload_cmd($channel_name, $outputDir, qq{$chat.json}, $vod_id);
     &PeePoo::printl(q{notice}, qq{\n(chat Download) executing $chatDownloadCmd\n});
     my ($chatDownload_executionStatus, $chatDownload_returnOutput, $chatDownload_returnCode) = &PeePoo::printxl($chatDownloadCmd);
@@ -323,7 +323,7 @@ sub live_trigger() {
             . qq{\n$chatDownload_returnOutput\n}
             . qq{\tReturn Code $chatDownload_returnCode\n}
             . qq{\n\nMoving the VOD to the destination folder and exiting out early\n});
-            &PeePoo::printxl(qq{mv -v $outputDir/'$video' $gcsMountPoint/$channel_name/'$video'});
+            &PeePoo::printxl(qq{mv -v $outputDir/'$video' $gcsMountPoint/$gcsBucketName/$channel_name/'$video'});
             return 1;
         }
     }
@@ -332,7 +332,7 @@ sub live_trigger() {
         . qq{\n$chatDownload_returnOutput\n}
         . qq{\tReturn Code $chatDownload_returnCode\n}
         . qq{\n\nMoving the VOD to the destination folder and exiting out early\n});
-        &PeePoo::printxl(qq{mv -v $outputDir/'$video' $gcsMountPoint/$channel_name/'$video'});
+        &PeePoo::printxl(qq{mv -v $outputDir/'$video' $gcsMountPoint/$gcsBucketName/$channel_name/'$video'});
         return 154;
     }
 
@@ -350,7 +350,7 @@ sub generate_ratio() {
         1080 => 422,
         720  => 281,
     );
-    if ($stream =~ m/Video\:.*?\s\d{3,4}x(\d{3,4})/) { 
+    if ($stream =~ m/Video\:.*?\s\d{3,4}x(\d{3,4})/) {
         my $height = $1;
         if (defined $chat_ratio{$height}) {
             return ($height, $chat_ratio{$height});
@@ -389,7 +389,7 @@ sub get_vod_id() {
     Log::Log4perl->easy_init($ERROR);
     my $url = qq{https://twitch.tv/$channel_name/videos?filter=archives&sort=time};
     my $js  = q{document.getElementsByClassName("ScTransformWrapper-sc-1wvuch4-1 gMwbGx")[0].firstChild.href.match(/\d{10,}/)[0]};
-    
+
     Log::Log4perl->easy_init($ERROR);
     my $mech = WWW::Mechanize::Chrome->new(
         launch_exe    => q{chromium},
@@ -452,7 +452,7 @@ sub transfer() {
     my $channel_name = shift;
     my $localPath    = shift;
     my $localPath_fn = shift;
-    my $transfer_dir = qq{$gcsMountPoint/$channel_name/};
+    my $transfer_dir = qq{$gcsMountPoint/$gcsBucketName/$channel_name/};
 
     my $is_mounted = &setup_transient_storage($channel_name);
     if ($is_mounted) {
@@ -481,7 +481,7 @@ sub setup_transient_storage() {
         &PeePoo::printxl(qq{gcsfuse --key-file $authorization --log-file $gcsLogFile --debug_gcs --debug_fuse --debug_http --implicit-dirs $gcsBucketName $gcsMountPoint});
         my $is_mounted = qx{ls -l $gcsMountPoint/ | wc -l | tr -d "\n"};
         if ($is_mounted) {
-            &PeePoo::printl(q{info}, qq{Mounting completed.}); 
+            &PeePoo::printl(q{info}, qq{Mounting completed.});
             return $is_mounted;
         }
         else {
